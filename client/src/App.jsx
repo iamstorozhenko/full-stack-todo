29b23ee56ea493eec37ae1fd3106ef9a2e8cd4a9
iamstorozhenko <iamantonstorozhenko@gmail.com>
2022-12-16 15:42:04 +0200

    added dark theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
+import { createContext, useState } from "react";
+
 import InputTodo from "./components/InputTodo";
 import ListTodos from "./components/ListTodos";
+import Switch from "react-switch";
+
+const themeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="container">
-      <InputTodo />
-      <ListTodos />
-    </div>
+    <themeContext.Provider>
+      <div className="container" id={theme}>
+        <Switch
+          onChange={toggleTheme}
+          checked={theme === "dark"}
+          checkedIcon={false}
+          uncheckedIcon={false}
+        />
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </themeContext.Provider>
   );
 }
 
